71d2d90ca2b8142e4a06fb2a283aeae8db3d7a0c
rdx <31172722+rd-xx@users.noreply.github.com>
2023-05-01 16:32:42 +0200

    Initial Commit

---
+++ Pet Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pet Generator
+//
+//  Created by Vitor Sousa on 01/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
